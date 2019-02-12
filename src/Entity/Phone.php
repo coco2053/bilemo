e04bf89a4ecb3991294af93b436b28e83f71b6c9
Bastien Vacherand <coco2053@hotmail.com>
2019-02-12 17:15:09 +0100

    Fixes #1; Phone entity added

---
+++ src/Entity/Phone.php
@@ -0,0 +1,125 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\PhoneRepository")
+ */
+class Phone
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $brand;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $modelName;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $modelRef;
+
+    /**
+     * @ORM\Column(type="smallint")
+     */
+    private $memory;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $color;
+
+    /**
+     * @ORM\Column(type="float")
+     */
+    private $price;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getBrand(): ?string
+    {
+        return $this->brand;
+    }
+
+    public function setBrand(string $brand): self
+    {
+        $this->brand = $brand;
+
+        return $this;
+    }
+
+    public function getModelName(): ?string
+    {
+        return $this->modelName;
+    }
+
+    public function setModelName(string $modelName): self
+    {
+        $this->modelName = $modelName;
+
+        return $this;
+    }
+
+    public function getModelRef(): ?string
+    {
+        return $this->modelRef;
+    }
+
+    public function setModelRef(string $modelRef): self
+    {
+        $this->modelRef = $modelRef;
+
+        return $this;
+    }
+
+    public function getMemory(): ?int
+    {
+        return $this->memory;
+    }
+
+    public function setMemory(int $memory): self
+    {
+        $this->memory = $memory;
+
+        return $this;
+    }
+
+    public function getColor(): ?string
+    {
+        return $this->color;
+    }
+
+    public function setColor(string $color): self
+    {
+        $this->color = $color;
+
+        return $this;
+    }
+
+    public function getPrice(): ?float
+    {
+        return $this->price;
+    }
+
+    public function setPrice(float $price): self
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+}
