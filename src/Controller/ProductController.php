0c28874d879506a2b172e3ad3bf18f67e2c8c645
Bastien Vacherand <coco2053@hotmail.com>
2019-02-12 17:17:52 +0100

    Fixes #2; ProductController added

---
+++ src/Controller/ProductController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ProductController extends AbstractController
+{
+    /**
+     * @Route("/product", name="product")
+     */
+    public function index()
+    {
+        return $this->render('product/index.html.twig', [
+            'controller_name' => 'ProductController',
+        ]);
+    }
+}
