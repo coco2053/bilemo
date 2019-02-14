11d17420253271123bd880cf1a1b6bc99ea7ace0
Bastien Vacherand <coco2053@hotmail.com>
2019-02-14 11:01:00 +0100

    Fixes #5; show method added

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -6,6 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use FOS\RestBundle\Controller\Annotations as Rest;
 use App\Repository\PhoneRepository;
+use App\Entity\Phone;
 
 class ProductController extends AbstractController
 {
@@ -19,5 +20,22 @@ class ProductController extends AbstractController
         $phones = $repo->findAll();
 
         return $phones;
+    }
+
+    /**
+     * @Rest\Get(
+     *     path = "/products/{id}",
+     *     name = "product_show",
+     *     requirements = {"id"="\d+"}
+     * )
+     * @Rest\View
+     */
+    public function show(Phone $phone, $id)
+    {
+        if ($phone) {
+            return $phone;
+        }
+
+        return new Response('No product found with ID '.$id, Response::HTTP_NOT_FOUND);
     }
 }
